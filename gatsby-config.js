68854be70899564f11d23d51ad55210ac97ea9c3
Vincent (Anapher) <anapher@protonmail.com>
2021-09-23 12:23:15 +0200

    update packages, eslint, new prettier config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,156 +1,145 @@
 module.exports = {
-  siteMetadata: {
-    title: `Büro für Freiraumplanung`,
-    subtitle: "dipl.-ing. Franz Griebel",
-    description: `Büro für Freiraumplanung. Öffentliche Naturbäder, private Schwimmteiche, Wasserspielplätze, Grünanlagen, Gärten, und mehr.`,
-    author: `@gatsbyjs`,
-    lang: "de",
-    contact: {
-      mapSrc:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1265403.2034364091!2d8.678173374202933!3d51.72406088873035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bb15c5ede660eb%3A0x6831c71a90c6b6d2!2sIm%20Graben%209%2C%2034292%20Ahnatal!5e0!3m2!1sde!2sde!4v1600865734418!5m2!1sde!2sde",
-      address:
-        "Büro für Freiraumplanung\nIm Graben 9\n34292 Ahnatal\nHessen, Deutschland",
-      phone: "Franz Griebel\n05609 8097939\nMon - Fr, 8:00-18:00",
-      emailAddress: "info@griebel-naturbad.de",
-    },
-    components: {
-      appbar: {
-        navigation: [
-          {
-            title: "Projekte",
-            links: [
-              {
-                title: "Öffentliche Naturschwimmbäder",
-                url: "/projects/overview/public",
-              },
-              {
-                title: "Private Schwimmteiche",
-                url: "/projects/overview/privatePool",
-              },
-              {
-                title: "Wasserspielplätze/Sprühfelder",
-                url: "/projects/overview/waterPlayground",
-              },
-              {
-                title: "Eingriffs- und Ausgleichsplanung",
-                url: "/projects/overview/garden",
-              },
-              {
-                title: "Campingplätze",
-                url: "/projects/overview/garden",
-              },
-              {
-                title: "Grünanlagen, Gärten",
-                url: "/projects/overview/garden",
-              },
+   siteMetadata: {
+      title: `Büro für Freiraumplanung`,
+      subtitle: 'dipl.-ing. Franz Griebel',
+      description: `Büro für Freiraumplanung. Öffentliche Naturbäder, private Schwimmteiche, Wasserspielplätze, Grünanlagen, Gärten, und mehr.`,
+      author: `@gatsbyjs`,
+      lang: 'de',
+      contact: {
+         mapSrc:
+            'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1265403.2034364091!2d8.678173374202933!3d51.72406088873035!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bb15c5ede660eb%3A0x6831c71a90c6b6d2!2sIm%20Graben%209%2C%2034292%20Ahnatal!5e0!3m2!1sde!2sde!4v1600865734418!5m2!1sde!2sde',
+         address: 'Büro für Freiraumplanung\nIm Graben 9\n34292 Ahnatal\nHessen, Deutschland',
+         phone: 'Franz Griebel\n05609 8097939\nMon - Fr, 8:00-18:00',
+         emailAddress: 'info@griebel-naturbad.de',
+      },
+      components: {
+         appbar: {
+            navigation: [
+               {
+                  title: 'Projekte',
+                  links: [
+                     {
+                        title: 'Öffentliche Naturschwimmbäder',
+                        url: '/projects/overview/public',
+                     },
+                     {
+                        title: 'Private Schwimmteiche',
+                        url: '/projects/overview/privatePool',
+                     },
+                     {
+                        title: 'Wasserspielplätze/Sprühfelder',
+                        url: '/projects/overview/waterPlayground',
+                     },
+                     {
+                        title: 'Eingriffs- und Ausgleichsplanung',
+                        url: '/projects/overview/garden',
+                     },
+                     {
+                        title: 'Campingplätze',
+                        url: '/projects/overview/garden',
+                     },
+                     {
+                        title: 'Grünanlagen, Gärten',
+                        url: '/projects/overview/garden',
+                     },
+                  ],
+               },
+               {
+                  title: 'Referenzen',
+                  url: '/references',
+               },
+               {
+                  title: 'Kontakt',
+                  url: '/contact',
+               },
             ],
-          },
-          {
-            title: "Referenzen",
-            url: "/references",
-          },
-          {
-            title: "Kontakt",
-            url: "/contact",
-          },
-        ],
+         },
+         footer: {
+            copyright: 'dipl.-ing. Franz Griebel',
+         },
       },
-      footer: {
-        copyright: "dipl.-ing. Franz Griebel",
-      },
-    },
-    templates: {
-      projects: {
-        path: "/content/projects/",
-        pathPrefix: "projects",
-        template: "Project.tsx",
-        pagination: {
-          template: "ProjectsAll.tsx",
-          resultsPerPage: 6,
-        },
-        filters: {
-          type: {
-            pathPrefix: "projects/overview",
-            template: "ProjectType.tsx",
-          },
-        },
-      },
-      services: {
-        pathPrefix: "services",
-        template: "Service.tsx",
-      },
-    },
-  },
-  plugins: [
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/content`,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-        gatsbyRemarkPlugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 1200,
+      templates: {
+         projects: {
+            path: '/content/projects/',
+            pathPrefix: 'projects',
+            template: 'Project.tsx',
+            pagination: {
+               template: 'ProjectsAll.tsx',
+               resultsPerPage: 6,
             },
-          },
-        ],
+            filters: {
+               type: {
+                  pathPrefix: 'projects/overview',
+                  template: 'ProjectType.tsx',
+               },
+            },
+         },
+         services: {
+            pathPrefix: 'services',
+            template: 'Service.tsx',
+         },
       },
-    },
-    `gatsby-remark-images`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Büro für Freiraumplanung`,
-        short_name: `büro-für-freiraumplanung`,
-        start_url: `/`,
-        background_color: `#27ae60`,
-        theme_color: `#27ae60`,
-        display: `minimal-ui`,
-        icon: `content/icon.png`, // This path is relative to the root of the site.
+   },
+   plugins: [
+      `gatsby-plugin-react-helmet`,
+      {
+         resolve: `gatsby-source-filesystem`,
+         options: {
+            name: `data`,
+            path: `${__dirname}/content`,
+         },
       },
-    },
-    "gatsby-plugin-typescript",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-material-ui",
-    // {
-    //   resolve: `gatsby-plugin-typography`,
-    //   options: {
-    //     pathToConfigModule: `src/utils/typography`,
-    //   },
-    // },
-    "gatsby-plugin-react-leaflet",
-    {
-      resolve: "gatsby-plugin-prefetch-google-fonts",
-      options: {
-        fonts: [
-          {
-            family: "Roboto",
-          },
-          {
-            family: "Work Sans",
-            variants: ["800"],
-          },
-        ],
+      {
+         resolve: `gatsby-plugin-mdx`,
+         options: {
+            extensions: [`.mdx`, `.md`],
+            gatsbyRemarkPlugins: [
+               {
+                  resolve: `gatsby-remark-images`,
+                  options: {
+                     maxWidth: 1200,
+                  },
+               },
+            ],
+         },
       },
-    },
-    {
-      resolve: "gatsby-plugin-react-svg",
-      options: {
-        rule: {
-          include: /assets/,
-        },
+      `gatsby-remark-images`,
+      `gatsby-transformer-sharp`,
+      `gatsby-plugin-sharp`,
+      {
+         resolve: `gatsby-plugin-manifest`,
+         options: {
+            name: `Büro für Freiraumplanung`,
+            short_name: `büro-für-freiraumplanung`,
+            start_url: `/`,
+            background_color: `#27ae60`,
+            theme_color: `#27ae60`,
+            display: `minimal-ui`,
+            icon: `content/icon.png`, // This path is relative to the root of the site.
+         },
       },
-    },
-  ],
-  pathPrefix: "/griebel-naturbad",
+      'gatsby-plugin-sass',
+      'gatsby-plugin-material-ui',
+      // {
+      //   resolve: `gatsby-plugin-typography`,
+      //   options: {
+      //     pathToConfigModule: `src/utils/typography`,
+      //   },
+      // },
+      {
+         resolve: 'gatsby-plugin-google-fonts',
+         options: {
+            fonts: [`Roboto`, `Work Sans\:800`],
+         },
+      },
+      {
+         resolve: 'gatsby-plugin-react-svg',
+         options: {
+            rule: {
+               include: /assets/,
+            },
+         },
+      },
+   ],
+   pathPrefix: '/griebel-naturbad',
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,54 +1,54 @@
 const path = require(`path`);
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
-  const { createPage } = actions;
+   const { createPage } = actions;
 
-  // Get the template config settings.
-  const {
-    data: {
-      site: {
-        siteMetadata: { templates },
+   // Get the template config settings.
+   const {
+      data: {
+         site: {
+            siteMetadata: { templates },
+         },
       },
-    },
-  } = await graphql(`
-    {
-      site {
-        siteMetadata {
-          templates {
-            projects {
-              path
-              pathPrefix
-              template
-              pagination {
-                template
-                resultsPerPage
-              }
-              filters {
-                type {
-                  pathPrefix
-                  template
-                }
-              }
+   } = await graphql(`
+      {
+         site {
+            siteMetadata {
+               templates {
+                  projects {
+                     path
+                     pathPrefix
+                     template
+                     pagination {
+                        template
+                        resultsPerPage
+                     }
+                     filters {
+                        type {
+                           pathPrefix
+                           template
+                        }
+                     }
+                  }
+                  services {
+                     pathPrefix
+                     template
+                  }
+               }
             }
-            services {
-              pathPrefix
-              template
-            }
-          }
-        }
+         }
       }
-    }
-  `);
+   `);
 
-  const createMarkdownPages = async ({
-    regex,
-    template,
-    pathPrefix = "",
-    paginate = false,
-    resultsPerPage,
-    paginationTemplate,
-  }) => {
-    const result = await graphql(`
+   const createProjectPages = async ({
+      regex,
+      template,
+      pathPrefix = '',
+      paginate = false,
+      resultsPerPage,
+      paginationTemplate,
+   }) => {
+      const result = await graphql(`
 			{
 				allMdx(
 					filter: { fileAbsolutePath: { regex: "${regex}" } }
@@ -57,172 +57,184 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 					edges {
 						node {
 							frontmatter {
-                id
+                        id
 							}
+                     fileAbsolutePath
 						}
 					}
 				}
 			}
 		`);
 
-    // Report any errors if they occurred.
-    if (result.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query.`);
-      return;
-    }
+      // Report any errors if they occurred.
+      if (result.errors) {
+         reporter.panicOnBuild(`Error while running GraphQL query.`);
+         return;
+      }
 
-    // Iterate through the query results to create individual pages.
-    const pages = result.data.allMdx.edges;
+      // Iterate through the query results to create individual pages.
+      const pages = result.data.allMdx.edges;
 
-    // Calculate the number of paginated results pages.
-    const totalPages = Math.ceil(pages.length / resultsPerPage);
+      // Calculate the number of paginated results pages.
+      const totalPages = Math.ceil(pages.length / resultsPerPage);
 
-    const staticPages = pages.map(({ node: { frontmatter: { id } } }) =>
-      createPage({
-        path: `${pathPrefix}/${id}`,
-        component: path.resolve(`${__dirname}/src/templates/${template}`),
-        context: {
-          id,
-        },
-      })
-    );
+      const getDirectoryOfFilename = (path) => path.substring(0, path.lastIndexOf('/') + 1);
 
-    return !paginate
-      ? staticPages
-      : Promise.all([
-          ...staticPages,
-          Array.from({ length: totalPages }).map((_, i) =>
+      const staticPages = pages.map(
+         ({
+            node: {
+               frontmatter: { id },
+               fileAbsolutePath,
+            },
+         }) =>
             createPage({
-              path: `${pathPrefix}/page/${i + 1}`,
-              component: path.resolve(
-                `${__dirname}/src/templates/${paginationTemplate}`
+               path: `${pathPrefix}/${id}`,
+               component: path.resolve(`${__dirname}/src/templates/${template}`),
+               context: {
+                  id,
+                  carouselDirectory: getDirectoryOfFilename(fileAbsolutePath) + 'carousel/',
+               },
+            }),
+      );
+
+      return !paginate
+         ? staticPages
+         : Promise.all([
+              ...staticPages,
+              Array.from({ length: totalPages }).map((_, i) =>
+                 createPage({
+                    path: `${pathPrefix}/page/${i + 1}`,
+                    component: path.resolve(`${__dirname}/src/templates/${paginationTemplate}`),
+                    context: {
+                       limit: templates.projects.pagination.resultsPerPage,
+                       skip: i * templates.projects.pagination.resultsPerPage,
+                       totalPages,
+                       currentPage: i + 1,
+                    },
+                 }),
               ),
-              context: {
-                limit: templates.projects.pagination.resultsPerPage,
-                skip: i * templates.projects.pagination.resultsPerPage,
-                totalPages,
-                currentPage: i + 1,
-              },
-            })
-          ),
-        ]);
-  };
+           ]);
+   };
 
-  // Find all of the tags used in posts and create search result pages.
-  const createProjectType = async ({ pathPrefix = "", template }) => {
-    const result = await graphql(`
-      {
-        allMdx {
-          group(field: frontmatter___type) {
-            type: fieldValue
-            totalCount
-          }
-        }
-      }
-    `);
-
-    // Report any errors if they occurred.
-    if (result.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query.`);
-      return;
-    }
-
-    // Create a page for each tag.
-    const types = result.data.allMdx.group;
-    return types.map(async ({ type }) =>
-      createPage({
-        path: `${pathPrefix}/${type}`,
-        component: path.resolve(`${__dirname}/src/templates/${template}`),
-        context: {
-          type,
-        },
-      })
-    );
-  };
-
-  const createServiceInfo = async ({ pathPrefix = "", template }) => {
-    const result = await graphql(`
-      {
-        allMdx(filter: { fileAbsolutePath: { regex: "/services/" } }) {
-          edges {
-            node {
-              id
-              fileAbsolutePath
+   // Find all of the tags used in posts and create search result pages.
+   const createProjectType = async ({ pathPrefix = '', template }) => {
+      const result = await graphql(`
+         {
+            allMdx {
+               group(field: frontmatter___type) {
+                  type: fieldValue
+                  totalCount
+               }
             }
-          }
-        }
+         }
+      `);
+
+      // Report any errors if they occurred.
+      if (result.errors) {
+         reporter.panicOnBuild(`Error while running GraphQL query.`);
+         return;
       }
-    `);
 
-    // Report any errors if they occurred.
-    if (result.errors) {
-      reporter.panicOnBuild(`Error while running GraphQL query.`);
-      return;
-    }
+      // Create a page for each tag.
+      const types = result.data.allMdx.group;
+      return types.map(async ({ type }) =>
+         createPage({
+            path: `${pathPrefix}/${type}`,
+            component: path.resolve(`${__dirname}/src/templates/${template}`),
+            context: {
+               type,
+            },
+         }),
+      );
+   };
 
-    // Create a page for each tag.
-    const edges = result.data.allMdx.edges;
-    return edges.map(async ({ node: { id, fileAbsolutePath } }) => {
-      const type = /\/services\/([^\/]+)/.exec(fileAbsolutePath)[1];
+   const createServiceInfo = async ({ pathPrefix = '', template }) => {
+      const result = await graphql(`
+         {
+            allMdx(filter: { fileAbsolutePath: { regex: "/services/" } }) {
+               edges {
+                  node {
+                     id
+                     fileAbsolutePath
+                  }
+               }
+            }
+         }
+      `);
 
-      return createPage({
-        path: `${pathPrefix}/${type}`,
-        component: path.resolve(`${__dirname}/src/templates/${template}`),
-        context: {
-          type,
-          id,
-        },
+      // Report any errors if they occurred.
+      if (result.errors) {
+         reporter.panicOnBuild(`Error while running GraphQL query.`);
+         return;
+      }
+
+      // Create a page for each tag.
+      const edges = result.data.allMdx.edges;
+      return edges.map(async ({ node: { id, fileAbsolutePath } }) => {
+         const type = /\/services\/([^\/]+)/.exec(fileAbsolutePath)[1];
+
+         return createPage({
+            path: `${pathPrefix}/${type}`,
+            component: path.resolve(`${__dirname}/src/templates/${template}`),
+            context: {
+               type,
+               id,
+            },
+         });
       });
-    });
-  };
+   };
 
-  const createArticles = async () => {
-    const {
-      data: {
-        allMdx: { edges },
-      },
-    } = await graphql(`
-      {
-        allMdx(filter: { fileAbsolutePath: { regex: "/articles/" } }) {
-          edges {
-            node {
-              frontmatter {
-                id
-                template
-              }
+   const createArticles = async () => {
+      const {
+         data: {
+            allMdx: { edges },
+         },
+      } = await graphql(`
+         {
+            allMdx(filter: { fileAbsolutePath: { regex: "/articles/" } }) {
+               edges {
+                  node {
+                     frontmatter {
+                        id
+                        template
+                     }
+                  }
+               }
             }
-          }
-        }
-      }
-    `);
+         }
+      `);
 
-    return edges.map(({ node: { frontmatter: { id, template } } }) =>
-      createPage({
-        path: `${id}`,
-        component: path.resolve(`${__dirname}/src/templates/${template}`),
-        context: {
-          id,
-        },
-      })
-    );
-  };
+      return edges.map(
+         ({
+            node: {
+               frontmatter: { id, template },
+            },
+         }) =>
+            createPage({
+               path: `${id}`,
+               component: path.resolve(`${__dirname}/src/templates/${template}`),
+               context: {
+                  id,
+               },
+            }),
+      );
+   };
 
-  return await Promise.all([
-    // Create individual blog post pages and paginated results pages for mdx files in src/content/posts.
-    createMarkdownPages({
-      regex: templates.projects.path,
-      pathPrefix: templates.projects.pathPrefix,
-      template: templates.projects.template,
-      resultsPerPage: templates.projects.pagination.resultsPerPage,
-      paginationTemplate: templates.projects.pagination.template,
-      paginate: true,
-    }),
+   return await Promise.all([
+      createProjectPages({
+         regex: templates.projects.path,
+         pathPrefix: templates.projects.pathPrefix,
+         template: templates.projects.template,
+         resultsPerPage: templates.projects.pagination.resultsPerPage,
+         paginationTemplate: templates.projects.pagination.template,
+         paginate: true,
+      }),
 
-    // Create pages for each frontmatter tag used in src/content/posts with paginated result pages.
-    createProjectType(templates.projects.filters.type),
+      // Create pages for each frontmatter tag used in src/content/posts with paginated result pages.
+      createProjectType(templates.projects.filters.type),
 
-    // createServiceInfo(templates.services),
+      // createServiceInfo(templates.services),
 
-    createArticles(),
-  ]);
+      createArticles(),
+   ]);
 };
