43be0dac118567349aa3d426a35152eb9545f9e2
Vincent (Anapher) <anapher@protonmail.com>
2022-03-23 14:47:42 +0100

    fix project type translation

--- src/utils/project-type-translation.tsx
+++ src/utils/project-type-translation.tsx
@@ -1,8 +1,8 @@
 const projectTypeTranslation: { [key: string]: string } = {
   "oeffentliche-baeder": "Öffentliche Naturbäder",
   "private-schwimmteiche": "Private Schwimmteiche",
-  wasserspielplaetze: "Wasserspielplätze",
-  "eingriffs-und-ausgleichsplanung": "Eingriffs- und Ausgleichsplanung",
+  "wasserspielplaetze-spruehfelder": "Wasserspielplätze/Sprühfelder",
+  "eingriffs-ausgleichsplanung": "Eingriffs- und Ausgleichsplanung",
   campingplaetze: "Campingplätze",
   "gruenanlagen-gaerten": "Grünanlagen, Gärten",
 };
